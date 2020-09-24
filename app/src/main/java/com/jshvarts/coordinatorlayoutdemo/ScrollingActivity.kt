f2660334ac5aa31d19c13f72accd43e4b64a9a5c
James Shvarts <jshvarts@gmail.com>
2020-09-24 16:58:19 -0400

    Add Coordinator layout in Activity

---
+++ app/src/main/java/com/jshvarts/coordinatorlayoutdemo/ScrollingActivity.kt
@@ -0,0 +1,34 @@
+package com.jshvarts.coordinatorlayoutdemo
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_scrolling)
+    setSupportActionBar(toolbar)
+    toolbar_layout.title = title
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    menuInflater.inflate(R.menu.menu_scrolling, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+
+    return when (item.itemId) {
+      R.id.action_settings -> true
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+}
